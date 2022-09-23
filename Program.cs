b4bb5e6a0d12fcd8026949ec92f4efc1bbeada1a
gabrielmop <gabriel_moop@hotmail.com>
2022-09-23 11:35:59 -0300

    Escopo Inicial

--- Program.cs
+++ Program.cs
@@ -6,7 +6,53 @@ namespace CalculaIMC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string nome = "Gabriel";
+            string sexo = "Masculino";
+            int idade = 26;
+            float altura = 1.79f;
+            float peso = 76.4f;
+            string categoria = ""; 
+
+
+            if(idade < 12)
+            {
+
+                categoria = "infantil";
+
+            }
+            if(idade >= 12 && idade <= 20)
+            {
+
+                categoria = "juvenil";
+
+            }
+            if (idade >= 21 && idade <= 65){
+
+                categoria = "Adulto";
+            }
+            if (idade > 65) {
+
+                categoria = "idoso";
+            }
+
+
+                double imc = peso / (altura * altura);
+
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura);
+            Console.WriteLine(peso);
+            Console.WriteLine(categoria);
+
+
+
+
+
+
+
+
+
         }
     }
 }
